e6af915317f706b88e5cf3dab3bd58abfad9c5c6
frank <ramosfp88@gmail.com>
2021-03-16 14:13:27 +0800

    html updates

--- js/main.js
+++ js/main.js
@@ -5,5 +5,24 @@ const menuIcon = document.querySelector('.menu-icon');
 
 const timeLine = new TimelineMax();
 
-timeLine.fromTo(hero, 1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut })
-.fromTo(hero,1.5, {width: "100%"}, {width: "80%", ease: Power2.easeInOut });
+timeLine.fromTo(
+    hero,
+    1,
+    {height: "0%"},
+    {height: "80%",
+    ease: Power2.easeInOut
+})
+.fromTo(
+    hero,
+    1.5,
+    {width: "100%"},
+    {width: "80%",
+    ease: Power2.easeInOut
+})
+.fromTo(
+    slider,
+    1.2,
+    {x: "-100%"},
+    {x: "0%",
+    ease: Power2.easeInOut
+})
