cb0175ed4b0c7b30edaf5ebd943ce2844042c2a3
frank <ramosfp88@gmail.com>
2021-08-24 07:57:12 +0800

    updates

--- adon-signature/js/script.js
+++ adon-signature/js/script.js
@@ -1,6 +1,2 @@
-// const body = document.body;
-
-// const newDiv = document.createElement('div');
-// newDiv.innerText = "Sample text";
-// body.append(newDiv);
-// console.log(body);
+const body = document.body;
+body.style.backgroundColor = "black"
